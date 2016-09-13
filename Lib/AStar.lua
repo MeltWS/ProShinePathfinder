fe4e7d7858b18c7587ffa29231b1799e8d865b8d
shakadak <nathanael.pineau@gmail.com>
2016-09-13 22:02:12 +0200

    need testing

---
+++ Lib/AStar.lua
@@ -0,0 +1,42 @@
+local PQ = require "PriorityQueue"
+local HOF = require "HOF"
+
+local aStar = HOF.curry(function(expand, cost, heuristic, goal, start)
+	local open = PQ.new()
+	local closed = {}
+	local cameFrom = {}
+	local tCost = {}
+
+	open:insert(0, start)
+	cameFrom[start] = nil
+	tCost[start] = 0
+	for current in pop, open do
+		if goal(current) then
+			backtrack(cameFrom, current)
+		else
+			closed[current] = true
+			for neighbor in expand(current) do
+				if not closed[neighbor] then
+					local tmpCost = tCost[current] + cost(current, neighbor)
+					if tCost[neighbor] == nil or tmpCost < tCost[neighbor] then
+						cameFrom[neighbor] = current
+						tCost[neighbor] = tmpCost
+						open:insert(tmpCost + heuristic(neighbor), neighbor)
+					end
+				end
+			end
+		end
+	end
+	return nil
+end)
+
+local function backtrack(last, cameFrom)
+	path = {}
+	path[last] = nil
+	for to, from in pairs(cameFrom) do
+		path[from] = to
+	end
+	return path
+end
+
+return {aStar = aStar}
