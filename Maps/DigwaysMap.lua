c44b7b542d929d7650c3eae51edd97f58f7b7f1e
MeltG <melt.wildstar@gmail.com>
2016-09-18 04:25:19 +0200

    add digways map

---
+++ Maps/DigwaysMap.lua
@@ -0,0 +1,21 @@
+
+local Digways = {}
+
+-- Kanto
+Digways["Route 2"] = {["x"] = 30, ["y"] = 31}
+Digways["Route 11"] = {["x"] = 12, ["y"] = 12}
+Digways["Route 3"] = {["x"] = 74, ["y"] = 16}
+Digways["Route 4"] = {["x"] = 9, ["y"] = 20}
+
+Digways["Route 10"] = {["x"] = 2, ["y"] = 44}
+Digways["Route 10"] = {["x"] = 9, ["y"] = 9} -- issue 2 digway on the same map
+
+-- Johto
+Digways["Route 31"] = {["x"] = 53, ["y"] = 10}
+Digways["Route 45"] = {["x"] = 14, ["y"] = 5}
+Digways["Route 32"] = {["x"] = 10, ["y"] = 143}
+Digways["Route 33"] = {["x"] = 14, ["y"] = 13}
+Digways["Route 44"] = {["x"] = 73, ["y"] = 9}
+Digways["Blackthorn City"] = {["x"] = 49, ["y"] = 16}
+
+return Digways
