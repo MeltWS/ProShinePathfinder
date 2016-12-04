6f7b0072282d8524967ec467201ee9b16dc2398e
MeltG <melt.wildstar@gmail.com>
2016-12-04 17:20:54 +0100

    fix pokecenters

--- Pathfinder/Lib/MoveTo/Pokecenter.lua
+++ Pathfinder/Lib/MoveTo/Pokecenter.lua
@@ -18,17 +18,17 @@ local function useNearestPokecenter(map)
     if map == "Indigo Plateau Center" then
         return assert(talkToNpcOnCell(4, 22), "Failed to talk to Nurse on Cell 4/22")
     elseif string.find(map, "Seafoam") and getMoney() > 1500 then
-        if moveTo(map, "Seafoam B4F_D") then
+        if pf.moveTo(map, "Seafoam B4F_D") then
             return true
         end
         return assert(talkToNpcOnCell(59,13), "Failed to talk to Nurse on Cell 59/13")
     elseif string.find(map, "Rock Tunnel") then
-        if MoveTo(map, "Rock Tunnel 1_C") then
+        if pf.moveTo(map, "Rock Tunnel 1_C") then
             return true
         end
         return assert(talkToNpcOnCell(32,31), "Failed to talk to Nurse on Cell 32/31")
     elseif Lib.inTable({"Moon","Moon 1F","Moon B1F"}, map) then
-        if MoveTo(map, "Moon") then
+        if pf.moveTo(map, "Moon") then
             return true
         end
         return assert(talkToNpcOnCell(16,48), "Failed to talk to NPC on Cell 16/48")
